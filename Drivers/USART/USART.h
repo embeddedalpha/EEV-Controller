75b0aee9cda63822ed0e0e3706ecc8a94b94353b
embeddedalpha <kunalsalvi63@gmail.com>
2024-09-21 21:59:12 -0500

    Updated USART.h

--- Drivers/USART/USART.h
+++ Drivers/USART/USART.h
@@ -42,7 +42,7 @@ int8_t USART_Init(USART_Config *config);
 void USART_TX_Byte(USART_Config *config, uint16_t data);
 uint16_t USART_RX_Byte(USART_Config *config);
 int8_t USART_TX_Buffer(USART_Config *config, uint8_t *tx_buffer, uint16_t length);
-int8_t USART_RX_Buffer(USART_Config *config, uint8_t *rx_buffer, uint16_t length);
+int8_t USART_RX_Buffer(USART_Config *config, uint8_t *rx_buffer, uint16_t length, bool circular_buffer_enable);
 
 
 
