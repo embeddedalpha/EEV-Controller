7c480bdc265af4e83ed43ec4e58c29c1be3fbecc
embeddedalpha <kunalsalvi63@gmail.com>
2024-09-22 22:17:18 -0500

    Updated repo

--- Debug/Src/main.d
+++ Debug/Src/main.d
@@ -7,7 +7,8 @@ Src/main.o: ../Src/main.c ../Inc/main.h ../Inc/stm32f407xx.h \
  B:/STM32F407V_Firmware_C_Lang/EEV_Controller/Drivers/GPIO/GPIO_Defs.h \
  B:/STM32F407V_Firmware_C_Lang/EEV_Controller/Drivers/USART/USART_Defs.h \
  B:/STM32F407V_Firmware_C_Lang/EEV_Controller/Drivers/DMA/DMA.h \
- B:/STM32F407V_Firmware_C_Lang/EEV_Controller/Drivers/DMA/DMA_Defs.h
+ B:/STM32F407V_Firmware_C_Lang/EEV_Controller/Drivers/DMA/DMA_Defs.h \
+ B:/STM32F407V_Firmware_C_Lang/EEV_Controller/Drivers/Console/Console.h
 ../Inc/main.h:
 ../Inc/stm32f407xx.h:
 ../Inc/core_cm4.h:
@@ -23,3 +24,4 @@ B:/STM32F407V_Firmware_C_Lang/EEV_Controller/Drivers/GPIO/GPIO_Defs.h:
 B:/STM32F407V_Firmware_C_Lang/EEV_Controller/Drivers/USART/USART_Defs.h:
 B:/STM32F407V_Firmware_C_Lang/EEV_Controller/Drivers/DMA/DMA.h:
 B:/STM32F407V_Firmware_C_Lang/EEV_Controller/Drivers/DMA/DMA_Defs.h:
+B:/STM32F407V_Firmware_C_Lang/EEV_Controller/Drivers/Console/Console.h:
