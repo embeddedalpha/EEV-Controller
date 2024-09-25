efbd235745e13870d494efc70ebf118c5c61ddc3
embeddedalpha <kunalsalvi63@gmail.com>
2024-09-24 22:17:43 -0500

    Updated Modbus Driver

--- Communication/MODBus/MODBus.h
+++ Communication/MODBus/MODBus.h
@@ -9,7 +9,8 @@
 #define MODBUS_MODBUS_H_
 
 #include "main.h"
-#include "MODBus.h"
+#include "USART/USART.h"
+#include "MODBus_Defs.h"
 
 #define turnaround_delay_ms 100
 #define response_time_out_ms 1000
@@ -23,25 +24,30 @@ typedef struct ModBus_Request{
 	uint16_t crc;
 }ModBus_Request;
 
-typedef struct ModBus_Response{
+typedef struct ModBus_Frame{
 
-	uint8_t slave_address;
-	uint8_t function_code;
-	uint8_t byte_count;
-	uint8_t data[256];
-	uint16_t crc;
-}Modbus_Response;
+    uint8_t slaveAddress;
+    uint8_t functionCode;
+    uint16_t registerAddress;
+    uint16_t numberOfRegisters;
+    uint8_t requestData[256];  // Buffer to hold incoming request data
+    uint8_t responseData[256]; // Buffer to hold outgoing response data
+    uint16_t crc;              // CRC for RTU mode
+}ModBus_Frame;
 
 typedef struct ModBus_Config{
 
-
-
-
+	uint8_t slave_address;
+	uint8_t type;
+	uint32_t buadrate;
 
 }Modbus_Config;
 
+
+
 unsigned short ModBus_CRC16(uint16_t *packet, uint16_t len);
 
+int8_t Modbus_Init(Modbus_Config *config);
 
 
 
--- Communication/MODBus/MODBus_Defs.h
+++ Communication/MODBus/MODBus_Defs.h
@@ -10,7 +10,7 @@
 
 #include "main.h"
 
-static const struct MODBus_Function_Field{
+static const struct MODBus_Functions{
 
 	uint8_t Read_Coil;
 	uint8_t Read_Discrete_Inputs;
@@ -24,7 +24,7 @@ static const struct MODBus_Function_Field{
 
 	uint8_t Read_Write_Mulitple_Registers;
 
-}MODBus_Function_Field = {
+}MODBus_Functions = {
 
 		.Read_Coil              = 0x01,
 		.Read_Discrete_Inputs   = 0x02,
@@ -36,6 +36,37 @@ static const struct MODBus_Function_Field{
 		.Write_Multiple_Registers = 0x10,
 		.Read_Write_Mulitple_Registers = 0x17,
 };
+
+static const struct MODBus_Exception_Code{
+
+	uint8_t Illegal_Function;
+	uint8_t Illegal_Data_Address;
+	uint8_t Illegal_Data_Value;
+	uint8_t Slave_Device_Failure;
+	uint8_t Acknowledge;
+	uint8_t Slave_Device_Busy;
+	uint8_t Negative_Acknowledge;
+	uint8_t Memory_Parity_Error;
+	uint8_t Gateway_Path_Unavailable;
+	uint8_t Gateway_Target_Device_Failed_to_Respond;
+
+
+}MODBus_Exception_Code =
+		{
+				.Illegal_Function = 0x01,
+				.Illegal_Data_Address = 0x02,
+				.Illegal_Data_Value = 0x03,
+				.Slave_Device_Failure = 0x04,
+				.Acknowledge = 0x05,
+				.Slave_Device_Busy = 0x06,
+				.Negative_Acknowledge = 0x07,
+				.Memory_Parity_Error = 0x08,
+				.Gateway_Path_Unavailable = 0x0A,
+				.Gateway_Target_Device_Failed_to_Respond = 0xB
+
+		};
+
+
 
 
 #endif /* MODBUS_MODBUS_DEFS_H_ */
