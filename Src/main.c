4f01b3c77d53cb4c6c2849fcfbb5b631f59c607e
embeddedalpha <kunalsalvi63@gmail.com>
2024-10-01 22:53:38 -0500

    Updated Repo

--- Src/main.c
+++ Src/main.c
@@ -1,45 +1,28 @@
 #include "main.h"
-#include "math.h"
-#include "USART/USART.h"
 #include "GPIO/GPIO.h"
-#include "Console/Console.h"
-
-#include "../Signal_Processing/PID/PID.h"
-
-#define Superheat_SetPoint 10
-
-PID_Controller_Typedef EEV;
-
-uint16_t Sensor_Array[2];
+#include "USART/USART.h"
 
 
+USART_Config Modbus_USART;
 
-void TIM6_DAC_IRQHandler(void)
+#define Modbus_RX_Buffer_Length 20
+
+uint8_t Modbus_RX_Buffer_1[Modbus_RX_Buffer_Length];
+
+uint16_t Modbus_Message_length = 0;
+
+void USART1_IRQHandler(void)
 {
-	TIM6 -> SR &= ~TIM_SR_UIF;
 
-	float pressure = Get_Pressure();
-	float temp_kel = Get_Temperature();
+		Modbus_USART.USART_DMA_Instance_RX.Request.Stream -> CR &= ~DMA_SxCR_EN;
 
-	float saturation_temp = Get_Saturation_Temp_From_Pressure(pressure, R454B_Saturation_Table, R545B_Table_Size);
+		Modbus_Message_length = Modbus_RX_Buffer_Length - Modbus_USART.USART_DMA_Instance_RX.Request.Stream -> NDTR;
 
-	float superheat_cal = temp_kel - saturation_temp;
+		static int temp = 0;
+		temp = Modbus_USART.Port -> SR;
+		USART_RX_Buffer(&Modbus_USART, &Modbus_RX_Buffer_1[0], Modbus_RX_Buffer_Length, 0);
 
-	PID_Controller_Update(&EEV, superheat_cal);
-
-}
-
-void General_Timer_Update_Init(void)
-{
-	RCC -> APB1ENR |= RCC_APB1ENR_TIM6EN;
-	TIM6->PSC = 8400-1;
-	TIM6->ARR = 100-1;
-	TIM6 -> EGR |= TIM_EGR_UG;
-	TIM6->CR1 |= TIM_CR1_URS;
-	TIM6->DIER |=  TIM_DIER_UIE;
-	TIM6->CR1 |= TIM_CR1_CEN;
-	NVIC_SetPriority(TIM6_DAC_IRQn,1);
-	NVIC_EnableIRQ(TIM6_DAC_IRQn);
+		USART_TX_Buffer(&Modbus_USART, &Modbus_RX_Buffer_1[0], Modbus_Message_length);
 }
 
 
@@ -48,30 +31,31 @@ int main(void)
 	MCU_Clock_Setup();
 	Delay_Config();
 
-	EEV.T_Sampling_Time = 1/100.0f;
-	EEV.Limit_Max = 2000;
-	EEV.Limit_Min = 0;
-	EEV.tau = 0.001;
-	EEV.setpoint = Superheat_SetPoint;
+	Modbus_USART.Port = USART1;
+	Modbus_USART.RX_Pin = USART1_RX_Pin.PB7;
+	Modbus_USART.TX_Pin = USART1_TX_Pin.PB6;
+	Modbus_USART.baudrate = 9600;
+	Modbus_USART.dma_enable =  USART_Configuration.DMA_Enable.RX_Enable | USART_Configuration.DMA_Enable.TX_Enable;
+	Modbus_USART.hardware_flow = USART_Configuration.Hardware_Flow.Disable;
+	Modbus_USART.interrupt = USART_Configuration.Interrupt_Type.IDLE_Enable |  USART_Configuration.Interrupt_Type.Receiver_Empty_Enable;
+	Modbus_USART.mode = USART_Configuration.Mode.Asynchronous;
+	Modbus_USART.parity = USART_Configuration.Parity_Type.Disable;
+	Modbus_USART.stop_bits = USART_Configuration.Stop_Bits.Bit_1;
 
-	PID_Controller_Init(&EEV);
-
-
-
-
-//	Console_Init(9600);
+	USART_Init(&Modbus_USART);
+	USART_RX_Buffer(&Modbus_USART, &Modbus_RX_Buffer_1[0], Modbus_RX_Buffer_Length, 0);
 
 
 
 	GPIO_Pin_Init(GPIOD, 12, GPIO_Configuration.Mode.General_Purpose_Output, GPIO_Configuration.Output_Type.Push_Pull,
 			                 GPIO_Configuration.Speed.Very_High_Speed, GPIO_Configuration.Pull.No_Pull_Up_Down, GPIO_Configuration.Alternate_Functions.None);
 
-	General_Timer_Update_Init();
+
+//	General_Timer_Update_Init();
 
 
 	for(;;)
 	{
-
 
 		GPIO_Pin_High(GPIOD, 12);
 		Delay_s(1);
