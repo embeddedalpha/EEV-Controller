04d46ffd5f7cfa6134b9c962af2a09c3866b17bf
embeddedalpha <kunalsalvi63@gmail.com>
2024-10-03 21:30:32 -0500

    Updated working code

--- Src/main.c
+++ Src/main.c
@@ -1,84 +1,43 @@
 #include "main.h"
 #include "GPIO/GPIO.h"
-#include "USART/USART.h"
+#include "Console/Console.h"
 
-
-
-USART_Config Modbus_USART;
-
-#define Modbus_RX_Buffer_Length 200
-
-volatile uint8_t Modbus_RX_Buffer_1[Modbus_RX_Buffer_Length];
-
-volatile uint16_t Modbus_Message_length = 0;
-
-void USART1_IRQHandler(void)
-{
-
-	uint32_t temp1 = 0;
-	do {
-		Modbus_USART.USART_DMA_Instance_RX.Request.Stream -> CR &= ~DMA_SxCR_EN;
-		temp1 = Modbus_USART.USART_DMA_Instance_RX.Request.Stream -> CR & DMA_SxCR_EN;
-	} while (temp1 != 0);
-
-
-		Modbus_Message_length = Modbus_RX_Buffer_Length - Modbus_USART.USART_DMA_Instance_RX.Request.Stream -> NDTR;
-
-		static int temp = 0;
-		temp = Modbus_USART.Port -> SR;
-
-		if(Modbus_Message_length > 0)
-		{
-			USART_TX_Buffer(&Modbus_USART, &Modbus_RX_Buffer_1, Modbus_Message_length);
-
-			Modbus_Message_length = 0;
-		}
-		USART_RX_Buffer(&Modbus_USART, &Modbus_RX_Buffer_1[0], Modbus_RX_Buffer_Length, 0);
-}
 
 
 int main(void)
 {
 	MCU_Clock_Setup();
 	Delay_Config();
-
-	Modbus_USART.Port = USART1;
-	Modbus_USART.RX_Pin = USART1_RX_Pin.PB7;
-	Modbus_USART.TX_Pin = USART1_TX_Pin.PB6;
-	Modbus_USART.baudrate = 9600;
-	Modbus_USART.dma_enable =  USART_Configuration.DMA_Enable.RX_Enable | USART_Configuration.DMA_Enable.TX_Enable;
-	Modbus_USART.hardware_flow = USART_Configuration.Hardware_Flow.Disable;
-	Modbus_USART.interrupt = USART_Configuration.Interrupt_Type.IDLE_Enable |  USART_Configuration.Interrupt_Type.Receiver_Empty_Enable;
-	Modbus_USART.mode = USART_Configuration.Mode.Asynchronous;
-	Modbus_USART.parity = USART_Configuration.Parity_Type.Disable;
-	Modbus_USART.stop_bits = USART_Configuration.Stop_Bits.Bit_1;
-
-	USART_Init(&Modbus_USART);
-	USART_RX_Buffer(&Modbus_USART, &Modbus_RX_Buffer_1[0], Modbus_RX_Buffer_Length, 0);
-
+	Console_Init(9600);
 
 
 	GPIO_Pin_Init(GPIOD, 12, GPIO_Configuration.Mode.General_Purpose_Output, GPIO_Configuration.Output_Type.Push_Pull,
 			                 GPIO_Configuration.Speed.Very_High_Speed, GPIO_Configuration.Pull.No_Pull_Up_Down, GPIO_Configuration.Alternate_Functions.None);
 
 
-//	General_Timer_Update_Init();
 
+	volatile int ret_Scanf_temp = 0;
+	int number1 = 0;
+	int number2 = 0;
+	int result = 0;
 
 	for(;;)
 	{
+		printConsole("Please First Number X =  ");
+		ret_Scanf_temp = readConsole("%d",&number1);
+		printConsole("Please First Number Y =  ");
+		ret_Scanf_temp = readConsole("%d",&number2);
+		result = number1 + number2;
+		printConsole("Final Answer = %d\r\n",result);
+		printConsole("*******************************************************************\r\n");
 
-
-		GPIO_Pin_High(GPIOD, 12);
-		Delay_s(1);
-		GPIO_Pin_Low(GPIOD, 12);
-		Delay_s(1);
+//		GPIO_Pin_High(GPIOD, 12);
+//		Delay_s(1);
+//		GPIO_Pin_Low(GPIOD, 12);
+//		Delay_s(1);
 
 	}
 }
 
 
-
-//
-//
 
