b9ba37492a73c1f8c4aa198645ba3ce78f2a708e
embeddedalpha <kunalsalvi63@gmail.com>
2024-10-07 21:05:22 -0500

    Updated Project

--- Src/main.c
+++ Src/main.c
@@ -1,8 +1,10 @@
 #include "main.h"
-#include "USB/USB_FS/USB_FS_Driver.h"
+#include "USB/USB_FS/USB_Framework.h"
 
 
-USB_FS_Config testUSB;
+USB_Device testUSB;
+
+uint32_t buffer[8];
 
 
 
@@ -11,20 +13,15 @@ int main(void)
 	MCU_Clock_Setup();
 	Delay_Config();
 
+	testUSB.ptr_out_buffer = &buffer;
 
-	testUSB.Mode = USB_FS_Configurations.Modes.Device;
-	testUSB.SOF_Pin_Enable = 0;
-	testUSB.VBUS_Pin_Enable = 0;
-
-	USB_FS_Init(&testUSB);
-	USB_FS_Connect(&testUSB);
+	USB_Initialize(&testUSB);
 
 
 
 	for(;;)
 	{
-
-
+		USB_Poll();
 	}
 }
 
